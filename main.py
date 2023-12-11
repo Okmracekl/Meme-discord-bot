f53773cd00b12020c05e040deccd332f26c4e542
Okmracekl <150720064+Okmracekl@users.noreply.github.com>
2023-12-11 13:34:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import random
+
+description = "Bot of memes"
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command(name='meme')
+async def mem(ctx):
+    imagen = str(random.randint(1, 3))
+    with open("images/" + imagen + ".jpg", 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+    print("Sended ", imagen)
+
+
+bot.run("MTE4MzgzMTMxNTQ5NDIxOTc4Ng.GcFkiY.HmlQablXzKe73SNwrmZRk3BJCsPoITUSNQwlNo")
